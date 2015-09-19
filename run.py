a7c87529e3709a2252c35020394147e269f909d2
Jason Brooks <jason.brooks@yale.edu>
2015-09-19 12:16:35 -0700

    Initial commit

---
+++ run.py
@@ -0,0 +1,15 @@
+import time
+import requests
+
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return "hello world"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
